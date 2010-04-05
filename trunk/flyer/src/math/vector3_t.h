72d423a0deb2a876b6b665ac8d80a0fe5cbd2964
krzychuch <krzychuch@c37d027b-75c6-b93a-9d6d-d07ab28cfd6f>
2010-04-05 18:37:33 +0000

    added function for rotating vectors

--- trunk/flyer/src/math/vector3_t.h
+++ trunk/flyer/src/math/vector3_t.h
@@ -24,9 +24,15 @@ void vector3_normalize(vector3_t);
 void vector3_cross_product(vector3_t c, vector3_t a, vector3_t b);
 
 //sets vector coordinates
-void vector3_set(vector3_t v, float, float, float);
+void vector3_set(vector3_t v, float x, float y, float z);
+
+//sets vector to be equal to other vector
+void vector3_set_v(vector3_t v, vector3_t src);
 
 //inverts vector coordinates
 void vector3_invert(vector3_t);
+
+//rotates vector around specified axis
+void vector3_rotate(vector3_t result, vector3_t axis, float angle, vector3_t v);
 
 #endif /* VECTOR3_T_H_ */
