c519e9114da519001ab1a5e24c56c7be7ae24e90
krzychuch <krzychuch@c37d027b-75c6-b93a-9d6d-d07ab28cfd6f>
2010-04-06 11:21:41 +0000

    control handler beta added

--- flyer/src/game/control_handler_t.c
+++ flyer/src/game/control_handler_t.c
@@ -5,4 +5,32 @@
  *      Author: krzychu
  */
 
+/*
+ * functionality of translating SDL events into game events is
+ * implemented as returninig content of specific table.
+ */
+
 #include <control_handler_t.h>
+
+//finds event index
+int control_event_index(SDL_Event *ev){
+	if(ev->type == SDL_JOYAXISMOTION){
+		if(ev->jaxis.value == 0)
+			return CH_JOAXIS_0 + ev->jaxis.axis;
+		else if(ev->jaxis.value > 0)
+			return CH_JOAXIS_PLUS + ev->jaxis.axis;
+		else if(ev->jaxis.value < 0)
+			return CH_JOAXIS_MINUS + ev->jaxis.axis;
+	}
+	else if(ev->type == SDL_JOYBUTTONDOWN){
+		return CH_JOYBUTTON_DOWN + ev->jbutton.button;
+	}
+	else if(ev->type == SDL_JOYBUTTONUP){
+		return CH_JOYBUTTON_UP + ev->jbutton.button;
+	}
+}
+
+//translates event to player action
+char control_translate(control_handler_t h, SDL_Event *ev){
+	return h[control_event_index(ev)];
+}
