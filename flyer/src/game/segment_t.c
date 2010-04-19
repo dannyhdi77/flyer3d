9511abe2dbd2df315a782f85acc850385975ff2b
krzychuch <krzychuch@c37d027b-75c6-b93a-9d6d-d07ab28cfd6f>
2010-04-19 19:51:54 +0000

    i added smooth connectors beetween segments

--- flyer/src/game/segment_t.c
+++ flyer/src/game/segment_t.c
@@ -9,10 +9,27 @@
 
 //sets some basic properties
 void segment_init(segment_t* s, float length){
+	s->type = SEGMENT_NORMAL;
 	body_init(&s->obj);
 	s->length = length;
 	vector3_set(s->color, 1.0, 0.0, 0.0);	//default color
 }
+
+//initalizes segment as connector beetween two normal segments
+void segment_init_connector(segment_t* s, segment_t* prev, segment_t* next, float upr, float outr){
+	s->type = SEGMENT_CONNECTOR;
+	body_init(&s->obj);
+	//copy colors
+	vector3_set_v(s->color, prev->color);
+	vector3_set_v(s->next_color, next->color);
+	//set rotations
+	s->out_rotation = outr;
+	s->up_rotation = upr;
+	//find location
+	s->obj = prev->obj;
+	body_move_forward(&s->obj, -prev->length);
+}
+
 
 //deletes segment
 void segment_delete(segment_t* s){
@@ -26,19 +43,54 @@ void segment_display(segment_t* s){
 	int i;
 	float angle_step = 2*3.1415/SEGMENT_N_POINTS;
 	float x,y, angle = 0;
-	glBegin(GL_TRIANGLE_STRIP);
-	glColor3f(s->color[0], s->color[1], s->color[2]);
-	for(i = 0; i<SEGMENT_N_POINTS+2; i++){
-		x = SEGMENT_RADIUS*cos(angle);
-		y = SEGMENT_RADIUS*sin(angle);
+	if(s->type == SEGMENT_NORMAL){
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(s->color[0], s->color[1], s->color[2]);
+		for(i = 0; i<SEGMENT_N_POINTS+2; i++){
+			x = SEGMENT_RADIUS*cos(angle);
+			y = SEGMENT_RADIUS*sin(angle);
 
-		glNormal3f(-cos(angle), -sin(angle),0.0);
-		glVertex3f(x,y,0.0);
-		glVertex3f(x,y,-s->length);
+			glNormal3f(-cos(angle), -sin(angle),0.0);
+			glVertex3f(x,y,0.0);
+			glVertex3f(x,y,-s->length);
 
-		angle += angle_step;
+			angle += angle_step;
+		}
+		glEnd();
 	}
-	glEnd();
+	else{
+		glBegin(GL_TRIANGLE_STRIP);
+		vector3_t p,term, vx,vy;
+		//we obtain new axes
+		vector3_set(vx, 1.0, 0.0, 0.0);
+		vector3_set(vy, 0.0, 1.0, 0.0);
+		vector3_rotate(vx,vy,s->up_rotation);
+		vector3_rotate(vy,vx,s->out_rotation);
+
+		for(i = 0; i<SEGMENT_N_POINTS+2; i++){
+			x = SEGMENT_RADIUS*cos(angle);
+			y = SEGMENT_RADIUS*sin(angle);
+			glColor3f(s->color[0], s->color[1], s->color[2]);
+			glNormal3f(-cos(angle), -sin(angle),0.0);
+			glVertex3f(x,y,0.0);
+
+			glColor3f(s->next_color[0], s->next_color[1], s->next_color[2]);
+			vector3_set(p,0.0,0.0,-SEGMENT_GAP);
+
+			vector3_set_v(term, vx);
+			vector3_scale(term, x);
+			vector3_add(p, term);
+
+			vector3_set_v(term, vy);
+			vector3_scale(term, y);
+			vector3_add(p, term);
+
+			glVertex3f(p[0], p[1], p[2]);
+
+			angle += angle_step;
+		}
+		glEnd();
+	}
 	glPopMatrix();
 }
 
